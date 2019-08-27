48c8d3cedfcb38866756e53bb12b7d4de8e02163
tesla7 <77625961@qq.com>
2019-08-27 17:18:46 +0800

    init

---
+++ aiapp/chinese_ocr/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: yinhaozheng
+@software: PyCharm
+@file: __init__.py
+@time: 2019-08-21 17:46
+"""
+# import os
+# import sys
+#
+# __mtime__ = '2019-08-21'
+#
+# base_path = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(base_path)
+# sys.path.append(os.path.join(base_path, "ctpn"))
+
