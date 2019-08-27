06d118bfb1eaa50a5adb768a54fcd9cb8190044b
tesla7 <77625961@qq.com>
2019-08-27 17:05:00 +0800

    init

---
+++ ocrHtml/__init__.py
@@ -0,0 +1,12 @@
+# from glob import glob
+#
+# from aiapp.chinese_ocr.export import export
+#
+# if __name__ == '__main__':
+#
+#
+#        image_files = glob('/Users/yinhaozheng/PycharmProjects/ocrHtml/aiapp/taskFiles/*.*')
+#        print(image_files)
+#        export(image_files,
+#               "/Users/yinhaozheng/PycharmProjects/ocrHtml/aiapp/chinese_ocr/test_result/",
+#               "/Users/yinhaozheng/PycharmProjects/ocrHtml/aiapp/chinese_ocr/test_result/test.txt")
