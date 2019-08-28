58bc0d7c06805d7e65c4a1d7da93d91994844488
tesla7 <77625961@qq.com>
2019-08-28 14:29:44 +0800

    fix bug

--- ocrHtml/settings.py
+++ ocrHtml/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '4hpc%(lz$u4p9_o)l%ojqrs#7vanx0p#-4+%2@6x=dqb=-j%f-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
